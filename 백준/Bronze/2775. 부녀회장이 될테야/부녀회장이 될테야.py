91e421698982966666304d13891073ce9763e52e
ming018 <92374901+ming018@users.noreply.github.com>
2023-06-04 14:18:38 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,16 +1,22 @@
+# 미리 계산된 값을 저장할 2차원 배열 생성
 apart = [[0 for _ in range(15)] for _ in range(15)]
 
-for i in range(15) :
+# 초기 값 설정
+for i in range(15):
     apart[i][0] = 1
     apart[0][i] = i + 1
 
-for i in range(1, 15) :
-    for k in range(1, 15) :
-        apart[i][k] = sum(apart[i - 1][0 : k + 1])
+# 사람 수 계산
+for i in range(1, 15):
+    for k in range(1, 15):
+        apart[i][k] = apart[i][k - 1] + apart[i - 1][k]
 
 count = int(input())
-str_ = ""
-for _ in range(count) :
+output = []
+
+for _ in range(count):
     floor = int(input())
     num = int(input())
-    print(apart[floor][num - 1])
+    output.append(str(apart[floor][num - 1]))
+
+print("\n".join(output))
