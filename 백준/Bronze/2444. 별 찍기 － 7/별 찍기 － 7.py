ac765ce83fca85151af6112be61b9f46078a9dce
ming018 <92374901+ming018@users.noreply.github.com>
2023-06-06 12:13:34 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,7 @@
+num = int(input())
+
+for i in range(1, num + 1) :
+    print(" " * (num - i), "*" * i , "*" * (i - 1), sep = '')
+
+for i in range(num - 1, 0, -1) :
+    print(" " * (num - i), "*" * i , "*" * (i - 1), sep = '')
