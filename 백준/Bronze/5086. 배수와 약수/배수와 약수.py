f46cc718068165c34a2a46c7255690767529e9f5
ming018 <92374901+ming018@users.noreply.github.com>
2023-07-25 15:59:31 +0900

    [Bronze III] Title: 배수와 약수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.py
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -1,18 +1,15 @@
 import sys
 
-num1, num2 = map(int, sys.stdin.readline().split())
+while True :
+    num1, num2 = map(int, sys.stdin.readline().split())
 
-while(num1 != 0 and num2 != 0) :
-    if num1 >= num2 :
-        if num1 % num2 == 0 :
-            print('multiple')
-        else :
-            print('neither')
+    if num1 == num2 and num1 == 0 :
+        break
 
     else :
-        if num2 % num1 == 0 :
+        if num1 > num2 and num1 % num2 == 0 :
+            print('multiple')
+        elif num2 > num1 and num2 % num1 == 0:
             print('factor')
         else :
             print('neither')
-
-    num1, num2 = map(int, sys.stdin.readline().split())
