b970153576e77b9981861f32b153fa28255e43bf
ming018 <92374901+ming018@users.noreply.github.com>
2023-11-07 10:07:01 +0900

    [Bronze II] Title: 벌집, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -0,0 +1,21 @@
+limit = int(input())
+
+change = 5
+minimum = 2
+maximum = 7
+count = 2
+
+if limit == 1 :
+    print(count - 1)
+else :
+    while(True) :
+        1
+        if minimum <= limit and limit <= maximum :
+            print(count)
+            break
+
+        change += 6
+        minimum = maximum + 1
+        maximum = minimum + change
+        count += 1
+        
