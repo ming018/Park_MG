597c434195a322afb435b78123c4f93eca3d2d44
ming018 <92374901+ming018@users.noreply.github.com>
2024-02-01 11:01:57 +0900

    [Silver V] Title: 단어 정렬, Time: 76 ms, Memory: 35732 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,7 +1,9 @@
+import sys
+
 strs = []
 
-for _ in range(int(input())) :
-    strs.append(input())
+for _ in range(int(sys.stdin.readline())) :
+    strs.append(sys.stdin.readline().strip())
 strs = list(set(strs))
 strs.sort()
 strs.sort(key = len)
